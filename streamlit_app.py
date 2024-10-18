56c2a80994ad9b2935b46c808049a3eeef43aa42
ern-dev-it <it-support@erntec.com.au>
2024-10-18 01:59:31 +0000

    Just Testing ComboForm

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    st.title("Simple Web Form")
+
+    # Dropdown for the first selection
+    option1 = st.selectbox(
+        'Select Option 1:',
+        ['Option 1A', 'Option 1B', 'Option 1C']
+    )
+
+    # Dropdown for the second selection
+    option2 = st.selectbox(
+        'Select Option 2:',
+        ['Option 2A', 'Option 2B', 'Option 2C']
+    )
+
+    # Submit button
+    if st.button('Submit'):
+        st.success(f'Selected Value 1: {option1}, Selected Value 2: {option2}')
+
+if __name__ == "__main__":
+    main()

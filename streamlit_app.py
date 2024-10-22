a1104afb8fdb224e5b0f8e219bc5675e7eb57b83
ern-dev-it <it-support@erntec.com.au>
2024-10-22 22:23:39 +0000

    migrated SIO repo contents to it-support

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,39 @@
 import streamlit as st
+import pandas as pd
+from data_processing import process_data
 
-def main():
-    st.title("PAFD Configurator")
+# title element
+st.title('SIO report processor')
 
-    # Dropdown for the first selection
-    option1 = st.selectbox(
-        'Door Thickness:',
-        ['Standard', 'Fully Sealed',]
-    )
+# instruction element
+st.subheader('Input CSV')
 
-    # Dropdown for the second selection
-    option2 = st.selectbox(
-        'Select Option 2:',
-        ['Option 2A', 'Option 2B', 'Option 2C']
-    )
+# streamlit function - creates a widget that reads a file
+uploaded_file = st.file_uploader("Choose a file", type=["csv"])
 
-    # Submit button
-    if st.button('Submit'):
-        st.success(f'Selected Value 1: {option1}, Selected Value 2: {option2}')
+# for the output button - although st.dataframe below also allows csv download... just to make it easier
+def convert_df_to_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
 
-if __name__ == "__main__":
-    main()
+# Process data using function in data_processing
+if st.button("Process file"):
+    # handling odd cases, let's assume no one's going to upload other types of files...
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        processed_df = process_data(df)
+        # text element
+        st.write("Here is your report:")
+        # gives a preview of the output
+        st.dataframe(processed_df)
+
+        # converts the dataset into a csv
+        csv = convert_df_to_csv(processed_df)
+        # download
+        st.download_button(
+                label="Download Processed Data as CSV",
+                data=csv,
+                file_name='processed_data.csv',
+                mime='text/csv'
+            )
+    else:
+        st.write("Please upload a file before processing.")
